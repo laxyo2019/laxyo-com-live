6451ac9e2201270b598d1fba8fa54757e39a4c05
sameer <s.borde@laxyo.org>
2019-11-27 13:01:25 +0530

    dashboard icons

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
+use App\Models\Contact;
+use App\Models\Vendor;
 use Auth;
 
 class HomeController extends Controller
@@ -14,7 +17,12 @@ class HomeController extends Controller
 
   public function index()
   {
-    return view('admin.index');
+    $jobs     = Job::all()->count();
+    $vendors  = Vendor::all()->count();
+    $contacts = Contact::all()->count();
+    
+
+    return view('admin.index', compact('jobs', 'contacts', 'vendors'));
   }
 
   public function site_variables(){
